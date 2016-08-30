2799135791194b6d56a31684e98d1c5741e6beac
hui peng <penghui1012@gmail.com>
2016-08-30 17:08:53 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var modRewrite = require('connect-modrewrite');
+var watch = require('gulp-watch');
+var less = require('gulp-less');
+var shell = require('gulp-shell');
+var host = "http://localhost:8888";
+
+
+gulp.task('connect', function () {
+	connect.server({
+		root: './',
+		port: 10000,
+		livereload: false,
+		middleware: function (connect, o) {
+			return [
+				(function () {
+					var url = require('url');
+					var proxy = require('proxy-middleware');
+					var options = url.parse(host + '/pipeway');
+					options.route = '/pipeway';
+					return proxy(options);
+				})(),
+				modRewrite([
+					'!\\.html|\\.js|\\.css|\\.swf|\\.jp(e?)g|\\.png|\\.gif|\\.eot|\\.woff|\\.ttf|\\.svg$ /index.html'
+				])
+			];
+		}
+	});
+});
+
+gulp.task('dev', ['connect']);
