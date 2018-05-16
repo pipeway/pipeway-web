3a8a5c0e2d9ee7c568940448bb394ab7f3787b61
hui peng <penghui1012@gmail.com>
2018-05-16 17:59:47 +0800

    update

--- js/controllers/signin.js
+++ js/controllers/signin.js
@@ -1,17 +1,17 @@
 'use strict';
 
 /* Controllers */
-  // signin controller
+// signin controller
 app.controller('SigninFormController', ['$scope', '$http', '$state', '$cookies', '$cookieStore', '$location', 'api',
-    function($scope, $http, $state, $cookies, $cookieStore, $location, api) {
+    function ($scope, $http, $state, $cookies, $cookieStore, $location, api) {
         $scope.authError = null;
         $scope.Login = '登录中……';
-        $scope.login = function(store) {
+        $scope.login = function (store) {
             $scope.authError = null;
-            api.loginConfig().then(function(res) {
+            api.loginConfig().then(function (res) {
 
                 if (res) {
-                   window.location.href = res.loginUrl;
+                    window.location.href = res.loginUrl;
                 }
                 console.log(res);
                 /*if (res.success) {
@@ -20,21 +20,24 @@ app.controller('SigninFormController', ['$scope', '$http', '$state', '$cookies',
                 } else {
                     $scope.authError = '用户名或密码错误';
                 }*/
-            }).catch(function(r) {
+            }).catch(function (r) {
                 $scope.authError = 'Server Error';
             });
             return;
         };
-        function isOwnEmpty (obj){
-            for(var name in obj) {
-                if(obj.hasOwnProperty(name)) {
+
+        function isOwnEmpty(obj) {
+            for (var name in obj) {
+                if (obj.hasOwnProperty(name)) {
                     return false;
                 }
             }
             return true;
         }
 
-        var token = window.location.search;
+        var {
+            token
+        } = window.location.search;
         if (token) {
             $scope.Login = '登录中……';
             sessionStorage.setItem('token', token.slice(7));
@@ -45,9 +48,10 @@ app.controller('SigninFormController', ['$scope', '$http', '$state', '$cookies',
         } else {
             $scope.Login = '登录失败';
             setTimeout(function () {
-                 $scope.login();
+                $scope.login();
             }, 2000);
             return false;
         }
 
-  }]);
+    }
+]);
